625ccc41ed32def24d183fff7a31b8754575d832
felipe <felipee@gmail.com>
2026-04-30 00:27:12 -0500

    trabajando con flask

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, request, redirect
+import pyodbc
+
+app = Flask(__name__)
+
+# PAGINA PRINCIPAL
+@app.route("/")
+def inicio():
+    return render_template("index.html")
+
+
+# GUARDAR PRODUCTO
+@app.route("/guardar-producto", methods=["POST"])
+def guardar_producto():
+
+    nombre = request.form["nombre"]
+    precio = float(request.form["precio"])
+    cantidad = int(request.form["cantidad"])
+    descripcion = request.form["descripcion"]
+
+    conexion = pyodbc.connect(
+        'DRIVER={ODBC Driver 17 for SQL Server};'
+        'SERVER=DESKTOP-Q681RM2\\SQLEXPRESS;'
+        'DATABASE=InventarioAVA;'
+        'Trusted_Connection=yes;'
+    )
+
+    cursor = conexion.cursor()
+
+    cursor.execute("""
+        INSERT INTO Productos (nombre, precio, cantidad, descripcion)
+        VALUES (?, ?, ?, ?)
+    """, (nombre, precio, cantidad, descripcion))
+
+    conexion.commit()
+    conexion.close()
+
+    return redirect("/")
+
+
+app.run(debug=True)
