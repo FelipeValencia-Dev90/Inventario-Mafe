27b40e817101992dc3d94cafe201e902325416a0
felipe <felipee@gmail.com>
2026-05-31 19:29:19 -0500

    feat: agregar panel de predicción de acciones con IA y visualización inteligente de riesgos

--- ml/prediccion_stock.py
+++ ml/prediccion_stock.py
@@ -1,54 +1,86 @@
-import sys
-import os
+import pandas as pd
+import matplotlib.pyplot as plt
 
-# le enseña a Python a mirar una carpeta hacia atrás
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from sklearn.linear_model import LinearRegression
 
 from database.conexion import obtener_conexion
 
-import matplotlib.pyplot as plt
-
-import pandas as pd
 
 
 conexion = obtener_conexion()
 
 consulta = """
-        SELECT nombre, precio, cantidad
-        FROM Productos
-        WHERE activo = 1
+SELECT nombre, cantidad
+FROM Productos
+WHERE activo = 1
 """
 
 df = pd.read_sql(consulta, conexion)
 
-df["alerta_stock"] = df["cantidad"] < 30
+conexion.close()
 
+
+# SIMULACIÓN VENTAS DIARIAS
 df["ventas_diarias"] = [5, 20, 10, 8, 7, 3, 11, 5, 9, 2]
-df["dias_restantes"] = df["cantidad"] / df["ventas_diarias"]
 
-print("\n📊 PREDICCIÓN DE STOCK📊\n")
+
+# VARIABLE X (ventas)
+X = df[["ventas_diarias"]]
+
+# VARIABLE Y (stock)
+y = df["cantidad"]
+
+
+# CREAR MODELO IA
+modelo = LinearRegression()
+
+# ENTRENAR IA
+modelo.fit(X, y)
+
+
+# PREDICCIÓN
+prediccion = modelo.predict(X)
+
+df["prediccion_stock"] = prediccion
+
+df["riesgo"] = df["prediccion_stock"] < 40
+
+colores = []
+
+for riesgo in df["riesgo"]:
+
+    if riesgo:
+        colores.append("red")
+
+    else:
+        colores.append("green")
+
+
+print("\n🤖 ANÁLISIS INTELIGENTE INVENTARIO 🤖\n")
 
 print(df[[
     "nombre",
     "cantidad",
     "ventas_diarias",
-    "dias_restantes"
+    "prediccion_stock",
+    "riesgo"
 ]])
 
-plt.figure(figsize=(10,5))
+plt.figure(figsize=(12, 6))
 
-plt.bar(df["nombre"], df["cantidad"])
+plt.bar(
+    df["nombre"],
+    df["prediccion_stock"],
+    color=colores
+)
 
-plt.xticks(rotation=45)
-
-plt.title("Stock de productos")
+plt.title("📊 Predicción Inteligente de Stock")
 
 plt.xlabel("Productos")
+plt.ylabel("Predicción Stock")
 
-plt.ylabel("Cantidad")
+plt.xticks(rotation=45)
 
 plt.tight_layout()
 
 plt.show()
-
-conexion.close()
