0c62c21932117b56117ab81115362aadf7290181
felipe <felipee@gmail.com>
2026-05-31 20:02:35 -0500

    feat: implementar panel de control de pronóstico inteligente con IA y análisis de riesgos

--- ml/prediccion_stock.py
+++ ml/prediccion_stock.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 from sklearn.linear_model import LinearRegression
 
@@ -17,6 +18,8 @@ WHERE activo = 1
 
 df = pd.read_sql(consulta, conexion)
 
+df["dia"] = np.arange(len(df))
+
 conexion.close()
 
 
@@ -25,7 +28,7 @@ df["ventas_diarias"] = [5, 20, 10, 8, 7, 3, 11, 5, 9, 2]
 
 
 # VARIABLE X (ventas)
-X = df[["ventas_diarias"]]
+X = df[["dia", "ventas_diarias"]]
 
 # VARIABLE Y (stock)
 y = df["cantidad"]
@@ -36,6 +39,13 @@ modelo = LinearRegression()
 
 # ENTRENAR IA
 modelo.fit(X, y)
+
+futuro = pd.DataFrame({
+    "dia": [10, 11, 12, 13, 14],
+    "ventas_diarias": [8, 9 ,10, 11, 12]
+})
+
+predicciones_futuras = modelo.predict(futuro)
 
 
 # PREDICCIÓN
@@ -84,3 +94,9 @@ plt.xticks(rotation=45)
 plt.tight_layout()
 
 plt.show()
+
+print("\n🔮 PREDICCIÓN FUTURA DE STOCK 🔮\n")
+
+for i, prediccion in enumerate(predicciones_futuras):
+
+    print(f"Día {10 + i}: Stock estimado -> {prediccion:.2f}")
